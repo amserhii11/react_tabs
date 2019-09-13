49b1f002fd79a597f861faa88ccfa9f2a993f956
amserhii11 <amelin.serhii@gmail.com>
2019-09-13 22:38:17 +0300

    Updates

---
+++ precache-manifest.385b6e2b79c1648aee84f23a709e96b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a861f97faacbf78e8485ecb67b797c9",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "a25f954452e1e381625a",
+    "url": "/react_tabs/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/react_tabs/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "a25f954452e1e381625a",
+    "url": "/react_tabs/static/js/main.9815b7e7.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
